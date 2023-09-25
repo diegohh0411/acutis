3783847718649cd36607ac7b2aa9ff1f4e6f1655
diego <diegohh0411@gmail.com>
2023-09-25 09:08:20 -0600

    v0.1.0

---
+++ index.js
@@ -0,0 +1,5 @@
+const disclaimer = () => {
+    console.log('This is a NodeJS engine that helps parishes set up their own modern and aesthetical website. It is a work in progress and will soon be available fully.');
+};
+
+module.exports = disclaimer
